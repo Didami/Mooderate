0338d485b9d749a55b6d371089910502fbef5598
Diego Flores <diegofla0731@gmail.com>
2023-06-05 16:04:21 -0600

    Initial Commit

---
+++ Mooderate/ParentController.swift
@@ -0,0 +1,20 @@
+//
+//  ParentController.swift
+//  Mooderate
+//
+//  Created by Didami on 05/06/23.
+//
+
+import UIKit
+
+class ParentController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .systemBackground
+    }
+}
